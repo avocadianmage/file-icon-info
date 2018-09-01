6e68337ddf3a6494f8ee4a17b827e3d0d5affd17
avocadianmage <claytongotwals@gmail.com>
2018-09-01 03:30:06 -0500

    fix data output

--- src/file-icon-info.ts
+++ src/file-icon-info.ts
@@ -2,7 +2,7 @@ import * as child_process from 'child_process';
 
 export function getIcon(path: string, callback: (data: string) => any) {
     const proc = child_process.spawn(__dirname + '/FileIconInfo.exe');
-    proc.stdout.on('data', data => callback(data));
+    proc.stdout.on('data', data => callback(data.toString()));
     proc.stderr.on('data', err => console.log(err.toString()));
     proc.on('error', err => console.log(err.toString()));
     proc.stdin.write(path + '\n');
