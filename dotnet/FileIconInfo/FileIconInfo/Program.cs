546bb6cb4a741613dac5c1a13556e0404f72ba48
avocadianmage <claytongotwals@gmail.com>
2018-09-01 00:45:48 -0500

    Initial commit.

---
+++ dotnet/FileIconInfo/FileIconInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace FileIconInfo
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var path = Console.ReadLine();
+            if (!File.Exists(path))
+            {
+                Console.Error.WriteLine("Path {0} does not exist.", path);
+                return;
+            }
+
+            var icon = Icon.ExtractAssociatedIcon(path);
+            var base64String = convertIconToBase64String(icon);
+            Console.WriteLine(base64String);
+        }
+
+        static string convertIconToBase64String(Icon icon)
+        {
+            var converter = new ImageConverter();
+            var data = (byte[])converter.ConvertTo(
+                icon.ToBitmap(), typeof(byte[]));
+            return Convert.ToBase64String(data);
+        }
+    }
+}
---
+++ file-icon-info.js
@@ -0,0 +1,17 @@
+const child_process = require('child_process');
+const EventEmitter = require('events');
+
+function FileIconInfo() {
+    this.emitter = new EventEmitter();
+    this.getIcon = path => {
+        const proc = child_process.spawn('bin/FileIconInfo.exe');
+        proc.stdout.on('data', data => {
+            this.emitter.emit('icon-info', data.toString());
+        });
+        proc.stderr.on('data', err => console.log(err.toString()));
+        proc.on('error', err => console.log(err.toString()));
+        proc.stdin.write(path + '\n');
+    };
+}
+
+module.exports = new FileIconInfo();
