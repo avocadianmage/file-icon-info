faf034666564db08814f8aa51d07d1bdd63bd91e
avocadianmage <claytongotwals@gmail.com>
2018-09-01 03:22:57 -0500

    fix path to exe

--- src/file-icon-info.ts
+++ src/file-icon-info.ts
@@ -1,7 +1,7 @@
 import * as child_process from 'child_process';
 
 export function getIcon(path: string, callback: (data: string) => any) {
-    const proc = child_process.spawn(__dirname + '/bin/FileIconInfo.exe');
+    const proc = child_process.spawn(__dirname + '/FileIconInfo.exe');
     proc.stdout.on('data', data => callback(data));
     proc.stderr.on('data', err => console.log(err.toString()));
     proc.on('error', err => console.log(err.toString()));
