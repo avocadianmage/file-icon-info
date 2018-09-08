781f9b2a310a6b830a3d7061aa1368d8f557a16f
avocadianmage <claytongotwals@gmail.com>
2018-09-08 04:47:52 -0500

    use execFile so package works in asar

--- src/file-icon-info.ts
+++ src/file-icon-info.ts
@@ -1,9 +1,11 @@
-import * as child_process from 'child_process';
+import { execFile } from 'child_process';
+import * as path from 'path';
 
-export function getIcon(path: string, callback: (data: string) => any) {
-    const proc = child_process.spawn(__dirname + '/FileIconInfo.exe');
-    proc.stdout.on('data', data => callback(data.toString()));
-    proc.stderr.on('data', err => console.log(err.toString()));
-    proc.on('error', err => console.log(err.toString()));
-    proc.stdin.write(path + '\n');
+export function getIcon(exePath: string, callback: (data: string) => any) {
+    const helperBinaryPath = path.join(__dirname, 'FileIconInfo.exe');
+    const child = execFile(helperBinaryPath, (error, stdout, stderr) => {
+        if (error) throw error;
+        else callback(stdout);
+    });
+    child.stdin.write(`${exePath}\n`);
 }
